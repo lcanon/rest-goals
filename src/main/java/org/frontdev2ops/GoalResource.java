0d4670dae661a71e104c920fe12449dbf1bc93f2
Alberto <agarcia@datiobd.com>
2020-11-25 15:44:04 +0100

    Init

---
+++ src/main/java/org/frontdev2ops/GoalResource.java
@@ -0,0 +1,16 @@
+package org.frontdev2ops;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+
+@Path("/goal")
+public class GoalResource {
+
+  @GET
+  @Produces(MediaType.TEXT_PLAIN)
+  public String hello() {
+    return "hello";
+  }
+}
